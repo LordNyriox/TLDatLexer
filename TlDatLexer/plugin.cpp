9d9dd0c11f41c5067621f73397f9455dcb19bbf4
LordNyriox <LordNyriox@users.noreply.github.com>
2019-02-06 13:34:58 -0800

    Update License and Repository Information
    Added GPL headers, maintainer info, and bumped version to v0.5.0.

--- TlDatLexer/plugin.cpp
+++ TlDatLexer/plugin.cpp
@@ -1,3 +1,22 @@
+/*
+ * TlDatLexer Plugin for Notepad++.
+ * Copyright (C) 2013  Jason Newcomb
+ * Copyright (C) 2019  Ryan Medeiros <rm@spiralgate.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "stdafx.h"
 #include "plugin.h"
 
@@ -43,9 +62,9 @@ namespace {
 void __cdecl about() {
 	MessageBox(
 		nppHandle,
-		L"Author: Jason Newcomb.\n"
-		L"Source: http://bitbucket.org/Jarcho/tldatlexer\n\n"
-		L"This is free software; it comes with ABSOLUTELY NO WARRANTY.\n",
+		L"Author: Jason Newcomb and Ryan Medeiros.\n"
+		L"Source: https://github.com/LordNyriox/TLDatLexer\n\n"
+		L"License: GPL Version 3\n",
 		L"TLDatLexer (v" TEXT(TLDATLEXER_VERSION_STRING) L")",
 		MB_OK
 	);
