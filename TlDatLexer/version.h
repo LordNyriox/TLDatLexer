3b9be308eeade3bd541fc24112873c4e1c99b20c
LordNyriox <LordNyriox@users.noreply.github.com>
2019-02-06 13:55:00 -0800

    Update License and Repository Information (#6)
    Added GPL headers, maintainer info, and bumped version to v0.5.0.

--- TlDatLexer/version.h
+++ TlDatLexer/version.h
@@ -1,4 +1,23 @@
-#define TLDATLEXER_VERSION_STRING "0.4.3"
+/*
+ * TlDatLexer Plugin for Notepad++.
+ * Copyright (C) 2013  Jason Newcomb
+ * Copyright (C) 2019  Ryan Medeiros <rm@spiralgate.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define TLDATLEXER_VERSION_STRING "0.5.0"
 #define TLDATLEXER_VERSION_MAJOR 0
-#define TLDATLEXER_VERSION_MINOR 4
-#define TLDATLEXER_VERSION_PATCH 3
+#define TLDATLEXER_VERSION_MINOR 5
+#define TLDATLEXER_VERSION_PATCH 0
